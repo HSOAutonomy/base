bf435b5b578691e384f07d0c44c067bb7c426c7f
Klaus Dorer <klaus.dorer@hs-offenburg.de>
2017-04-07 09:17:46 +0200

    Moved first util classes from magma to here
    Moved classes under util into misc subpackage
    Moved gson library to runtime
    Adjusted copyright headers and launchers
    Added launcher for unit tests

---
+++ src/hso/autonomy/util/misc/FuzzyCompare.java
@@ -0,0 +1,181 @@
+/* Copyright 2008 - 2017 Hochschule Offenburg
+ * For a list of authors see README.md
+ * This software of HSOAutonomy is released under MIT License (see LICENSE).
+ */
+package hso.autonomy.util.misc;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+/**
+ * Implements a set of fuzzy compare operators for float and double data types.
+ *
+ * @author Ingo Schindler
+ */
+public class FuzzyCompare
+{
+	/**
+	 * Fuzzy "equal" ( == ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are equal
+	 * @return true if the both values are within the same range
+	 */
+	public static boolean eq(float reference, float compare, float range)
+	{
+		if (reference > compare) {
+			return reference - range <= compare;
+		}
+
+		return compare - range <= reference;
+	}
+
+	/**
+	 * Fuzzy "equal" ( == ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are equal
+	 * @return true if the both values are within the same range
+	 */
+	public static boolean eq(double reference, double compare, double range)
+	{
+		if (reference > compare) {
+			return reference - range <= compare;
+		}
+
+		return compare - range <= reference;
+	}
+
+	/**
+	 * Fuzzy "Equal" ( == ) comparison for vectors
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the vector values are considered to be equal
+	 * @return True if both vectors are equal
+	 */
+	public static boolean eq(Vector3D reference, Vector3D compare, double range)
+	{
+		if (reference == null && compare == null) {
+			return true;
+		}
+		if (reference == null || compare == null) {
+			return false;
+		}
+
+		if (!eq(reference.getX(), compare.getX(), range)) {
+			return false;
+		}
+		if (!eq(reference.getY(), compare.getY(), range)) {
+			return false;
+		}
+		return eq(reference.getZ(), compare.getZ(), range);
+	}
+
+	/**
+	 * Fuzzy "Greater Than or Equal" ( >= ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are considered equal
+	 * @return True if greater or equal, false if not
+	 */
+	public static boolean gte(float reference, float compare, float range)
+	{
+		return eq(reference, compare, range) || reference > compare;
+	}
+
+	/**
+	 * Fuzzy "Greater Than or Equal" ( >= ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are considered equal
+	 * @return True if greater or equal, false if not
+	 */
+	public static boolean gte(double reference, double compare, double range)
+	{
+		return eq(reference, compare, range) || reference > compare;
+	}
+
+	/**
+	 * Fuzzy "Lower Than or Equal" ( <= ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are considered equal
+	 * @return True if less than or equal, false if not
+	 */
+	public static boolean lte(float reference, float compare, float range)
+	{
+		return gte(compare, reference, range);
+	}
+
+	/**
+	 * Fuzzy "Lower Than or Equal" ( <= ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are considered equal
+	 * @return True if less than or equal, false if not
+	 */
+	public static boolean lte(double reference, double compare, double range)
+	{
+		return gte(compare, reference, range);
+	}
+
+	/**
+	 * Fuzzy "Greater Than" ( > ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare the lower of two values
+	 * @param range The range within the two values are considered equal
+	 * @return true if the first parameter is greater than the second one and the
+	 *         second one is not within the range of the first value
+	 */
+	public static boolean gt(float reference, float compare, float range)
+	{
+		return gte(reference, compare, range);
+	}
+
+	/**
+	 * Fuzzy "Greater Than" ( > ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare the lower of two values
+	 * @param range The range within the two values are considered equal
+	 * @return true if the first parameter is greater than the second one and the
+	 *         second one is not within the range of the first value
+	 */
+	public static boolean gt(double reference, double compare, double range)
+	{
+		return gte(reference, compare, range);
+	}
+
+	/**
+	 * Fuzzy "Lower Than" ( < ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are considered equal
+	 * @return True if lower, false if not
+	 */
+	public static boolean lt(float reference, float compare, float range)
+	{
+		return gte(compare, reference, range);
+	}
+
+	/**
+	 * Fuzzy "Lower Than" ( < ) comparison
+	 *
+	 * @param reference Reference value
+	 * @param compare Value to be compared to the reference
+	 * @param range The range within the two values are considered equal
+	 * @return True if lower, false if not
+	 */
+	public static boolean lt(double reference, double compare, double range)
+	{
+		return gte(compare, reference, range);
+	}
+}
---
+++ src/hso/autonomy/util/misc/GsonUtil.java
@@ -0,0 +1,61 @@
+/* Copyright 2008 - 2017 Hochschule Offenburg
+ * For a list of authors see README.md
+ * This software of HSOAutonomy is released under MIT License (see LICENSE).
+ */
+package hso.autonomy.util.misc;
+
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+
+public class GsonUtil
+{
+	private static Gson gson =
+			new GsonBuilder()
+					.registerTypeAdapter(Double.class,
+							(JsonSerializer<Double>) (src, typeOfSrc, context) -> {
+								if (src.isNaN() || src.isInfinite()) {
+									return new JsonPrimitive(src.toString());
+								}
+								return new JsonPrimitive(
+										new BigDecimal(src).setScale(3, RoundingMode.HALF_UP).doubleValue());
+							})
+					.registerTypeAdapter(Float.class,
+							(JsonSerializer<Float>) (src, typeOfSrc, context) -> {
+								if (src.isNaN() || src.isInfinite()) {
+									return new JsonPrimitive(src.toString());
+								}
+								return new JsonPrimitive(
+										new BigDecimal(src).setScale(3, RoundingMode.HALF_UP).floatValue());
+							})
+					.registerTypeAdapter(Duration.class,
+							(JsonSerializer<Duration>) (src, typeOfSrc,
+									context) -> new JsonPrimitive(DurationUtil.format(src)))
+					.registerTypeAdapter(Duration.class,
+							(JsonDeserializer<Duration>) (src, typeOfSrc,
+									context) -> DurationUtil.parse(src.getAsString()))
+					.setPrettyPrinting()
+					.serializeSpecialFloatingPointValues()
+					.create();
+
+	/**
+	 * JSON-Serialization with pretty-printing, float / double rounding to 3
+	 * decimals and support for special floating point values (NaN, infinity...).
+	 */
+	public static String toJson(Object src)
+	{
+		return gson.toJson(src);
+	}
+
+	public static <T> T fromJson(String src, Type typeOfT)
+	{
+		return gson.fromJson(src, typeOfT);
+	}
+}
