71e7fe988a8e67bb0de20765d3e2f578a2e147cd
Jens Fischer <jensfischer95@gmail.com>
2017-05-07 14:42:48 +0200

    GsonUtil: java.util.Optional handling

--- src/hso/autonomy/util/misc/GsonUtil.java
+++ src/hso/autonomy/util/misc/GsonUtil.java
@@ -5,16 +5,24 @@
 
 package hso.autonomy.util.misc;
 
-import java.lang.reflect.Type;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.Duration;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializer;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.util.Optional;
 
 public class GsonUtil
 {
@@ -42,6 +50,7 @@ public class GsonUtil
 					.registerTypeAdapter(Duration.class,
 							(JsonDeserializer<Duration>) (src, typeOfSrc,
 									context) -> DurationUtil.parse(src.getAsString()))
+					.registerTypeAdapterFactory(OptionalTypeAdapter.FACTORY)
 					.setPrettyPrinting()
 					.serializeSpecialFloatingPointValues()
 					.create();
@@ -58,5 +67,49 @@ public class GsonUtil
 	public static <T> T fromJson(String src, Type typeOfT)
 	{
 		return gson.fromJson(src, typeOfT);
+	}
+
+	public static class OptionalTypeAdapter<E> extends TypeAdapter<Optional<E>>
+	{
+		public static final TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
+			@Override
+			public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type)
+			{
+				Class<T> rawType = (Class<T>) type.getRawType();
+				if (rawType != Optional.class) {
+					return null;
+				}
+				final ParameterizedType parameterizedType = (ParameterizedType) type.getType();
+				final Type actualType = parameterizedType.getActualTypeArguments()[0];
+				final TypeAdapter<?> adapter = gson.getAdapter(TypeToken.get(actualType));
+				return new OptionalTypeAdapter(adapter);
+			}
+		};
+		private final TypeAdapter<E> adapter;
+
+		public OptionalTypeAdapter(TypeAdapter<E> adapter)
+		{
+			this.adapter = adapter;
+		}
+
+		@Override
+		public void write(JsonWriter out, Optional<E> value) throws IOException
+		{
+			if (value != null && value.isPresent()) {
+				adapter.write(out, value.get());
+			} else {
+				out.nullValue();
+			}
+		}
+
+		@Override
+		public Optional<E> read(JsonReader in) throws IOException
+		{
+			final JsonToken peek = in.peek();
+			if (peek != JsonToken.NULL) {
+				return Optional.ofNullable(adapter.read(in));
+			}
+			return Optional.empty();
+		}
 	}
 }
